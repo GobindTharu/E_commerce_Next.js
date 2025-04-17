4130f58ae5056837ccb239eb01f41bde9d9a0336
BalGobind Chaudhary <gobind9800@gmail.com>
2025-04-17 14:48:59 +0545

    DASHbord layout fixed responsive

--- ui/app/admin/Components/Header.jsx
+++ ui/app/admin/Components/Header.jsx
@@ -1,16 +1,16 @@
 import { Menu } from "lucide-react";
 import React from "react";
 
-const Header = () => {
+const Header = ({ toggleSidebar }) => {
   return (
     <section className="flex items-center gap-4 bg-white border-b p-4  bg-gradient-to-tr to-[#eff3f4] from-[#def3fd]">
-      <button className="flex md:hidden justify-center items-center">
-        <Menu/>
-
+      <button
+        onClick={toggleSidebar}
+        className="flex md:hidden justify-center items-center"
+      >
+        <Menu />
       </button>
-      <h1 className="text-xl font-semibold">
-        Dashboard
-        </h1>
+      <h1 className="text-xl font-semibold">Dashboard</h1>
     </section>
   );
 };
--- ui/app/admin/layout.jsx
+++ ui/app/admin/layout.jsx
@@ -1,22 +1,54 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Sidebar from "./Components/SideBar";
 import Header from "./Components/Header";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }) {
-  return (
-    <main className="flex">
-      <div className="hidden md:block">
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  const sidebarRef = useRef(null);
 
-      <Sidebar />
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    toggleSidebar();
+  }, [pathname]);
+
+  useEffect(() => {
+    function handleClickOutSideEvent(event) {
+      if (sidebarRef.current && !sidebarRef?.current?.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutSideEvent);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutSideEvent);
+    };
+  }, []);
+  return (
+    <main className="relative flex">
+      {/* Desktop size */}
+      <div className="hidden md:block">
+        <Sidebar />
+      </div>
+
+      {/* Mobile Size */}
+      <div
+        ref={sidebarRef}
+        className={`fixed md:hidden ease-in-out duration-500 transition-all
+        ${isOpen ? "translate-x-0" : "-translate-x-[280px]"}
+      `}
+      >
+        <Sidebar />
       </div>
       <section className="flex-1">
-        <Header />
-        <section className="flex-1 min-h-screen">
-
-        {children}
-        </section>
+        <Header toggleSidebar={toggleSidebar} />
+        <section className="flex-1 min-h-screen">{children}</section>
       </section>
     </main>
   );
